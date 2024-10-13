185922685523587c4a7798a7491f46a9e5851cd9
fdzvictor <victorfdzgrc@gmail.com>
2024-10-13 14:22:25 +0200

    Limpieza de nulos y estandarizacion columna-nombre

--- src/soporte_limpieza.py
+++ src/soporte_limpieza.py
@@ -1,8 +1,8 @@
 def traductor (dataframe):
  
 
-    cambio_nombre_columnas = {
-    columnas_acortadas = {
+ cambio_nombre_columnas = {
+  
     'CÓDIGO ÓRGÃO SUPERIOR': 'codigo_entidad_superior',
     'NOME ÓRGÃO SUPERIOR': 'nombre_entidad_superior',
     'CÓDIGO ÓRGÃO': 'codigo_organo',
@@ -21,7 +21,37 @@ def traductor (dataframe):
     'ANO EXERCÍCIO': 'ano_ejercicio'
 }
 
-}
-    dataframe.rename(columns = cambio_nombre_columnas,inplace= True) 
+
+ dataframe.rename(columns = cambio_nombre_columnas,inplace= True) 
     
-    return dataframe
+ return dataframe
+
+def anio_a_int (dataframe,año):
+
+#cambiamos nulos de año a año y cambiamos tipo a int (A mordiscos porque no sé optimizarlo :)
+ dataframe["anio_ejercicio"].fillna(año, inplace = True)
+ dataframe["anio_ejercicio"] = dataframe["anio_ejercicio"].astype(int)
+
+def codigo_nombre (dataframe,cod,nom):
+
+ if type(cod) == str and type(nom) == str:
+  #Sacamos valores unicos para renombrar
+  cols0y1 = dataframe [[cod,nom]].drop_duplicates().dropna().to_dict("records") 
+  #Creamos un codigo para mapear los nombres asignados a los códigos      
+  mapping = {item[cod]: item[nom] for item in cols0y1}
+
+  # Usamos map() para rellenar los valores nulos en 'nombre_entidad'
+  dataframe[nom] = dataframe[nom].fillna(dataframe[cod].map(mapping))
+
+  #Usamos la misma función de mapeo pero al revés, asignándole a cada código un nombre
+  mapping2 = {item[nom]: item[cod] for item in cols0y1}
+
+  # Usamos map() para rellenar los valores nulos en 'codigo_entidad'
+  dataframe[cod] = dataframe[cod].fillna(dataframe[nom].map(mapping2))
+  #Hemos igualado los nulos en ambas columnas y reducido los valores nulos totales sin perder observaciones
+  dataframe[[cod, nom]].shape
+
+  return dataframe
+
+ else:
+  print("Código y número han de ser strings!") 
